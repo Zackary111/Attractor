9bef3f257604029091382c88e961e34d4d37f1f2
Zackary111 <115322387+Zackary111@users.noreply.github.com>
2024-02-11 14:52:56 -0500

    Update Attractor.py

--- Attractor.py
+++ Attractor.py
@@ -1,10 +1,7 @@
 import matplotlib.pyplot as plt 
 from matplotlib.animation import FuncAnimation
-import numpy as np 
 
-# --- functions ---
-
-def Lorenz(xyz = [0,1,1.05],s=10, r=28, b=2.667,time = .01):
+def Lorenz(xyz=[0, 1, 1.05], s=10, r=28, b=2.667, time=0.01):
     """
     Parameters
     ----------
@@ -19,27 +16,25 @@ def Lorenz(xyz = [0,1,1.05],s=10, r=28, b=2.667,time = .01):
     3 lists containing the x-coordinates, y-coordinates and z-coordinates
     respectively.
     """
-    x=[xyz[0]]
-    y=[xyz[1]]
-    z=[xyz[2]]
-
-    for i in range(1,10000):
+    #Intializing the list of points
+    x, y, z = [xyz[0]], [xyz[1]], [xyz[2]]
+    for i in range(1, 10000):
+        #setting the current x,y,z cooridnate.
         current_x, current_y, current_z = x[-1], y[-1], z[-1]
 
-        x.append(current_x+ time*(s*(current_y  - current_x)))
-        y.append(current_y+  time*(r*current_x - current_y - current_x*current_z))
-        z.append(current_z + time*(current_x*current_y - b*current_z))
-    return x,y,z
+        #Updating the list of points with the next value
+        x.append(current_x + time * (s * (current_y - current_x)))
+        y.append(current_y + time * (r * current_x - current_y - current_x * current_z))
+        z.append(current_z + time * (current_x * current_y - b * current_z))
+    return x, y, z
 
 def init():
     return ln,
 
-def update(i):
+def animate(i):
     ln.set_data([ x_values[:i], y_values[:i]])
     ln.set_3d_properties( z_values[:i] )
     return ln,
-    
-# --- main ---
 
 fig = plt.figure(facecolor="black")
 ax = fig.add_subplot(projection='3d')
@@ -47,8 +42,8 @@ ax = fig.add_subplot(projection='3d')
 x_values, y_values, z_values = Lorenz()
 ln, = ax.plot(x_values, y_values, z_values)
 
-ani = FuncAnimation(fig, 
-                    update,
+animation = FuncAnimation(fig, 
+                    animate,
                     frames=len(x_values), 
                     init_func=init,
                     interval=.01,
